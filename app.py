fa6f1313376d61a81954396b35f7e04247500d9a
unknown <jagadyudhaawali@gmail.com>
2021-07-04 16:46:22 +0700

    add image

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request
 import urllib.parse
 import requests
 import json
-import pandas as pd
-import openpyxl
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36 OPR/58.0.3135.79'}
 
@@ -16,19 +14,18 @@ def index():
 @app.route("/getdata", methods = ['POST', 'GET'])
 def getdata():
     if request.method == 'POST':
-        form_data = request.form["name"]
-        tokopedia(form_data)
-        return render_template("getdata.html")
+        item_name = request.form["name"]
+        item_amount = request.form["amount"]
+        item_name_quote = urllib.parse.quote(item_name)
+        url = ("https://ace.tokopedia.com/search/product/v3?navsource=home&ob=23&st=product&q=" + item_name + "&rows=" + item_amount + "&start=0&full_domain=www.tokopedia.com&scheme=https&device=desktop&source=shop_product")
+        data = requests.get(url, headers=headers).content.decode("utf-8")
+        data = json.loads(data)
+        dict = data['data']['products']
+        pages = 1
+        return render_template('data.html', dict=dict)
 
-def tokopedia(item_name):
-    item_name_quote = urllib.parse.quote(item_name)
-    url = ("https://ace.tokopedia.com/search/product/v3?navsource=home&ob=3&st=product&q={}&rows=100&start=0&full_domain=www.tokopedia.com&scheme=https&device=desktop&source=shop_product").format(item_name_quote)
-    data = requests.get(url, headers=headers).content.decode("utf-8")
-    data = json.loads(data)
-    dict = data['data']['products']
-    df = pd.DataFrame(dict, columns = ['name', 'price'])
-    df = df.sort_values(by=['price'])
-    df.to_html('templates/getdata.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
